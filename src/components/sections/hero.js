b36612fda683f0fe6709bcb9c3c99c8a2cd733f8
Josbert Pedrol <josbert.pedrol@bassolsenergia.com>
2022-05-06 17:24:03 +0200

    changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -58,9 +58,9 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>josbert.cat</h1>;
+  const two = <h2 className="big-heading">Josbert Pedrol</h2>;
+  const three = <h3 className="medium-heading">Software developer</h3>;
   const four = (
     <>
       <p>
--- src/config.js
+++ src/config.js
@@ -1,27 +1,23 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: '',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/trebsoj',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://www.instagram.com/josbert.pedrol',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/josbert-pedrol-rojo-6a0a4745',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
   ],
 
   navLinks: [
