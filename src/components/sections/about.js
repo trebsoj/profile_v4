2808ed19040cc91533de925b302f422e912d8b02
Josbert Pedrol <josbert.pedrol@bassolsenergia.com>
2022-05-07 14:41:38 +0200

    changes

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -168,8 +168,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              // src="../../images/me.jpg"
-              src="../../images/profile.jpeg"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
