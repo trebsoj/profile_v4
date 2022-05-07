7000c52515a7c7334bbf5600b5a35bef5ba12961
Josbert Pedrol <josbert.pedrol@bassolsenergia.com>
2022-05-07 19:21:47 +0200

    changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,9 +266,9 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
-              Resume
-            </a>
+            {/*<a href="/resume.pdf" className="resume-link">*/}
+            {/*  Resume*/}
+            {/*</a>*/}
           </nav>
         </StyledSidebar>
       </div>
